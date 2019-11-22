7f04eee76d01abd0e737ba6e17723d92ad3c567c
Lorenzo Bercasio <lorenzo.s.bercasio@hotmail.com>
2019-11-22 17:40:33 +0800

    Changed to postgres db

--- webportfolio/settings.py
+++ webportfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Iam1019',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
